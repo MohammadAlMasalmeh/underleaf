5045c4e560a5ecb31efc4288b3d09ab4aa385c6f
Mohammad Al Masalmeh <malmasalmeh.2019@gmail.com>
2026-04-04 01:07:29 -0500

    Apply SecureFight defenses for db30d4d8-2d48-45c4-9e05-6145fa306ace

---
+++ backend/auth/jwt.py
@@ -0,0 +1,18 @@
+claims = jwt.decode(
+    token,
+    key=JWT_PUBLIC_KEY,
+    algorithms=['RS256'],
+    options={'require': ['exp', 'iat', 'sub']},
+)
+if claims.get('iss') != JWT_ISSUER:
+    raise HTTPException(status_code=401, detail='Invalid token issuer')
+claims = verify_refresh_token(refresh_token)
+token_id = claims.get('jti')
+if not token_id or token_store.is_revoked(token_id):
+    raise HTTPException(status_code=401, detail='Invalid refresh token')
+token_store.revoke(token_id)
+new_refresh = issue_refresh_token(user_id=claims['sub'])
+if invoice.account_id != current_user.account_id:
+    raise HTTPException(status_code=403, detail='Forbidden')
+@router.post('/search')
+@limiter.limit('60/minute')
